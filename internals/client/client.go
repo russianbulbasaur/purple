41390a34d5f104437a8b773718535867aa694bd9
bulbasaur <bulbasaur@localhost.localdomain>
2024-11-20 22:14:48 +0530

    Reading auxilary fields complete

--- internals/client/client.go
+++ internals/client/client.go
@@ -27,7 +27,7 @@ func NewClient(connection net.Conn, set func(string, interface{}, int64), get fu
 	rdbFile *rdb.RDBFile) *Client {
 	ctx, cancel := context.WithCancel(context.Background())
 	rdbReader := rdb.NewRDBReader(rdbFile)
-	rdbReader.ReadEOFSection()
+	rdbReader.ReadAuxiliaryFields()
 	return &Client{
 		connection,
 		resp.Init(),
--- internals/rdb/rdb_reader.go
+++ internals/rdb/rdb_reader.go
@@ -2,10 +2,16 @@ package rdb
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
 	"io"
 	"log"
 	"os"
+	"strconv"
 )
+
+const auxiliaryOPCode byte = 0xfa
+const dbOPCode byte = 0xfe
 
 type RDBReader struct {
 	osFile    *os.File
@@ -39,5 +45,108 @@ func (reader *RDBReader) ReadEOFSection() {
 }
 
 func (reader *RDBReader) ReadAuxiliaryFields() {
+	var auxiliaryMap map[string]string = make(map[string]string)
+	for {
+		readByte, err := reader.bufReader.ReadByte()
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+		if readByte == auxiliaryOPCode {
+			key := reader.length_decode_string()
+			log.Printf("Key : %s", key)
+			value := reader.length_decode_string()
+			auxiliaryMap[key] = value
+			log.Printf("%s : %s", key, value)
+		} else if readByte == dbOPCode {
+			err := reader.bufReader.UnreadByte()
+			if err != nil {
+				log.Fatalln(err)
+			}
+			break
+		}
+	}
+	log.Println("Read auxiliary fields")
+}
 
+func (reader *RDBReader) length_decode_string() string {
+	firstByte, err := reader.bufReader.ReadByte()
+	if err != nil {
+		log.Fatalln(err)
+		return ""
+	}
+	//checking the first two msbs
+	firstByteShifted := firstByte >> 6
+	switch int8(firstByteShifted) {
+	case 0:
+		//The next 6 bits represent the length
+		stringLength := (firstByte << 2) >> 2
+		var stringBytes []byte = make([]byte, stringLength)
+		_, err := reader.bufReader.Read(stringBytes)
+		if err != nil {
+			log.Fatalln(err)
+			return ""
+		}
+		value := string(stringBytes)
+		return value
+	case 1:
+		//Read one additional byte. The combined 14 bits represent the length
+	case 2:
+		//Discard the remaining 6 bits. The next 4 bytes from the stream represent the length
+	case 3:
+		//The next object is encoded in a special format. The remaining 6 bits indicate the format
+		remainingBits := (firstByte << 2) >> 2
+		result := reader.readInteger(remainingBits)
+		return strconv.Itoa(result)
+	}
+	return ""
+}
+
+func (reader *RDBReader) readInteger(remainingBits byte) int {
+	flag := int8(remainingBits)
+	var buffer []byte
+	if flag == 0 {
+		//8 bit int
+		buffer = make([]byte, 1)
+		_, err := reader.bufReader.Read(buffer)
+		log.Println(buffer)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var result int8
+		err = binary.Read(bytes.NewBuffer(buffer), binary.LittleEndian, &result)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return int(result)
+	} else if flag == 1 {
+		//8 bit int
+		buffer = make([]byte, 2)
+		_, err := reader.bufReader.Read(buffer)
+		log.Println(buffer)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var result int16
+		err = binary.Read(bytes.NewBuffer(buffer), binary.LittleEndian, &result)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return int(result)
+	} else if flag == 2 {
+		//8 bit int
+		buffer = make([]byte, 4)
+		_, err := reader.bufReader.Read(buffer)
+		log.Println(buffer)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		var result int32
+		err = binary.Read(bytes.NewBuffer(buffer), binary.LittleEndian, &result)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return int(result)
+	}
+	return -1
 }
